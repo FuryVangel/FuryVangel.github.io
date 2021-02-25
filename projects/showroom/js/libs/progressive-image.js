4922e761c3b01c3f8264ab7c0a2b9e10b88cba57
FuryVangel <edgecity23@gmail.com>
2021-02-25 18:33:36 +0300

    Update file

--- projects/showroom/js/libs/progressive-image.js
+++ projects/showroom/js/libs/progressive-image.js
@@ -1,104 +1,131 @@
+// progressive-image.js
+// by Craig Buckler, @craigbuckler
 if (window.addEventListener) window.addEventListener('load', function() {
 
   'use strict';
 
-  const EVENTS = ['scroll', 'wheel', 'mousewheel', 'resize']
+  // browser supported?
+  var body = document.body;
+  if (!body.getElementsByClassName || !body.querySelector || !body.classList || !body.getBoundingClientRect) return;
 
-  const Util = {
-    throttle(action, delay) {
-      let timeout = null
-      let lastRun = 0
-      return function() {
-        if (timeout) {
-          return
+  var
+          classReplace = 'replace',
+          classPreview = 'preview',
+          classReveal = 'reveal',
+          pItem = document.getElementsByClassName('progressive ' + classReplace),
+          rAF = window.requestAnimationFrame || function(f) { f(); },
+          timer;
+
+  // bind events
+  ['pageshow', 'scroll', 'resize'].forEach(function(h) {
+    window.addEventListener(h, throttle, { passive: true });
+  });
+
+  // DOM mutation observer
+  if (window.MutationObserver) {
+
+    var observer = new MutationObserver(throttle);
+    observer.observe(body, { subtree: true, childList: true, attributes: true });
+
+  }
+
+  // initial check
+  inView();
+
+
+  // throttle events, no more than once every 300ms
+  function throttle() {
+
+    timer = timer || setTimeout(function() {
+      timer = null;
+      inView();
+    }, 300);
+
+  }
+
+
+  // image in view?
+  function inView() {
+
+    if (pItem.length) rAF(function() {
+
+      var wH = window.innerHeight, cRect, cT, cH, p = 0;
+      while (p < pItem.length) {
+
+        cRect = pItem[p].getBoundingClientRect();
+        cT = cRect.top;
+        cH = cRect.height;
+
+        if (0 < cT + cH && wH > cT) {
+          loadFullImage(pItem[p]);
         }
-        const elapsed = Date.now() - lastRun
-        const context = this
-        const args = arguments
-        const runCallback = function() {
-          lastRun = Date.now()
-          timeout = false
-          action.apply(context, args)
-        }
-        if (elapsed >= delay) {
-          runCallback()
-        } else {
-          timeout = setTimeout(runCallback, delay)
-        }
+        else p++;
+
       }
-    },
-    on(el, ev, fn) {
-      el.addEventListener(ev, fn)
-    },
-    off(el, ev, fn) {
-      el.removeEventListener(ev, fn)
-    },
-  }
 
-  const events = (el, bind) => {
-    if(bind){
-      EVENTS.forEach(evt => {
-        Util.on(el, evt, lazy)
-      })
-    }else {
-      EVENTS.forEach(evt => {
-        Util.off(el, evt, lazy)
-      })
-    }
-  }
-
-  let windowHasBindEvents = false
-
-  const lazy = Util.throttle(()=>{
-    checkImage()
-  }, 300)
-
-  checkImage()
-
-
-  function checkImage() {
-    if(!windowHasBindEvents){
-      windowHasBindEvents = true
-      events(window, true)
-    }
-
-    const lazys = document.querySelectorAll('img.lazy')
-    const l = lazys.length
-    if(l>0){
-      for (let i = 0; i < l; i++) {
-        const rect = lazys[i].getBoundingClientRect()
-        if (rect.top < window.innerHeight && rect.bottom > 0 && rect.left < window.innerWidth && rect.right > 0) {
-          loadImage(lazys[i]);
-        }
-      }
-    }else {
-      windowHasBindEvents = false
-      events(window, false)
-    }
-  }
-
-  function loadImage(item) {
-    const img = new Image()
-    if (item.dataset) {
-      item.dataset.srcset && (img.srcset = item.dataset.srcset)
-      item.dataset.sizes && (img.sizes = item.dataset.sizes)
-    }
-    img.src = item.dataset.src;
-    img.className = 'origin';
-    img.onload = _ => {
-      item.classList.remove('lazy')
-      mountImage(item, img)
-    }
-  }
-
-  function mountImage(preview, img) {
-    const parent = preview.parentNode
-    parent.appendChild(img).addEventListener('animationend', function(e) {
-      preview.classList.remove('origin')
-      e.target.alt = preview.alt || '';
-      preview.classList.add('hide')
-      parent.removeChild(preview);
-      e.target.classList.remove('origin');
     });
+
   }
-})
+
+
+  // replace with full image
+  function loadFullImage(item, retry) {
+
+    // cancel monitoring
+    item.classList.remove(classReplace);
+
+    // fetch href and preview image
+    var
+            href = item.getAttribute('data-href') || item.href,
+            pImg = item.querySelector('img.' + classPreview);
+
+    if (!href || !pImg) return;
+
+    // load main image
+    var img = new Image(), ds = item.dataset;
+
+    if (ds) {
+      if (ds.srcset) img.srcset = ds.srcset;
+      if (ds.sizes) img.sizes = ds.sizes;
+    }
+
+    img.onload = addImg;
+
+    // load failure retry
+    retry = 1 + (retry || 0);
+    if (retry < 3) img.onerror = function() {
+      setTimeout(function() { loadFullImage(item, retry); }, retry * 3000);
+    };
+
+    img.src = href;
+
+    // replace image
+    function addImg() {
+
+      // disable link
+      if (href === item.href) {
+        item.style.cursor = 'default';
+        item.addEventListener('click', function(e) { e.preventDefault(); });
+      }
+
+      // apply image attributes
+      var imgClass = img.classList;
+      img.className = pImg.className;
+      imgClass.remove(classPreview);
+      imgClass.add(classReveal);
+      img.alt = pImg.alt || '';
+
+      rAF(function() {
+
+        // add full image
+        item.insertBefore(img, pImg.nextSibling).addEventListener('animationend', function() {
+
+          // remove preview image
+          item.removeChild(pImg);
+          imgClass.remove(classReveal);
+
+        });
+      });
+    }
+  }
+}, false);
